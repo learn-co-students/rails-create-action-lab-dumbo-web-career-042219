e08b0c44288957f3124bff10b9f018bb5c6737d8
Chloe <gingkolane@gmail.com>
2019-05-26 00:08:03 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,41 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(params.permit(:first_name, :last_name))
+
+    redirect_to student_path(@student)
+    # redirect_to @student
+  end
+
+  # def create
+  #   @student = Student.create(
+  #     {
+  #       first_name: params[:first_name],
+  #       last_name: params[:last_name]
+  #     }
+  #   )
+  #   redirect_to student_path(@student)
+  # end
+
+  private 
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
   end
 
 end
+
+
+# def create
+#   @student = Student.create(student_params)
+#   if @student.valid?
+#     redirect_to @student
+#   else
+#     flash[:errors] = @student.errors.full_messages
+#     redirect_to new_student_path
+#   end
+# end
